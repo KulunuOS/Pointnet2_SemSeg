94a8684f03706077e60df425b369ff82879f811d
KulunuOS <kulunuds@gmail.com>
2024-04-04 19:31:12 +0300

    .

---
+++ model/Semantic_Segmentation.py
@@ -0,0 +1,150 @@
+import torch
+import torch.nn as nn
+from lib.pointnet2_utils.pointnet2_modules import PointnetFPModule, PointnetSAModuleMSG
+import torch.nn.functional as F
+
+class Pointnet2MSG(nn.Module):
+    r"""
+        PointNet2 with multi-scale grouping
+        Semantic segmentation network that uses feature propogation layers
+
+        Parameters
+        ----------
+        input_channels: int = 6
+            Number of input channels in the feature descriptor for each point.  If the point cloud is Nx9, this
+            value should be 6 as in an Nx9 point cloud, 3 of the channels are xyz, and 6 are feature descriptors
+        use_xyz: bool = True
+            Whether or not to use the xyz position of a point as a feature
+    """
+
+    def __init__(
+        self, input_channels=6, use_xyz=True
+    ):
+        super(Pointnet2MSG, self).__init__()
+
+        self.SA_modules = nn.ModuleList()
+        c_in = input_channels
+        self.SA_modules.append(
+            PointnetSAModuleMSG(
+                npoint=2048,
+                radii=[0.0175, 0.025],
+                nsamples=[16, 32],
+                mlps=[[c_in, 16, 16, 32], [c_in, 32, 32, 64]],
+                use_xyz=use_xyz,
+            )
+        )
+        c_out_0 = 32 + 64
+
+        c_in = c_out_0
+        self.SA_modules.append(
+            PointnetSAModuleMSG(
+                npoint=1024,
+                radii=[0.025, 0.05],
+                nsamples=[16, 32],
+                mlps=[[c_in, 64, 64, 128], [c_in, 64, 96, 128]],
+                use_xyz=use_xyz,
+            )
+        )
+        c_out_1 = 128 + 128
+
+        c_in = c_out_1
+        self.SA_modules.append(
+            PointnetSAModuleMSG(
+                npoint=512,
+                radii=[0.05, 0.1],
+                nsamples=[16, 32],
+                mlps=[[c_in, 128, 196, 256], [c_in, 128, 196, 256]],
+                use_xyz=use_xyz,
+            )
+        )
+        c_out_2 = 256 + 256
+
+        c_in = c_out_2
+        self.SA_modules.append(
+            PointnetSAModuleMSG(
+                npoint=128,
+                radii=[0.1, 0.2],
+                nsamples=[16, 32],
+                mlps=[[c_in, 256, 256, 512], [c_in, 256, 384, 512]],
+                use_xyz=use_xyz,
+            )
+        )
+        c_out_3 = 512 + 512
+
+        self.FP_modules = nn.ModuleList()
+        self.FP_modules.append(PointnetFPModule(mlp=[256 + input_channels, 128, 128]))
+        self.FP_modules.append(PointnetFPModule(mlp=[512 + c_out_0, 256, 256]))
+        self.FP_modules.append(PointnetFPModule(mlp=[512 + c_out_1, 512, 512]))
+        self.FP_modules.append(PointnetFPModule(mlp=[c_out_3 + c_out_2, 512, 512]))
+
+    def _break_up_pc(self, pc):
+        xyz = pc[..., 0:3].contiguous()
+        features = pc[..., 3:].transpose(1, 2).contiguous() if pc.size(-1) > 3 else None
+
+        return xyz, features
+
+    def forward(self, pointcloud):
+        # type: (Pointnet2MSG, torch.cuda.FloatTensor) -> pt_utils.Seq
+        r"""
+            Forward pass of the network
+
+            Parameters
+            ----------
+            pointcloud: Variable(torch.cuda.FloatTensor)
+                (B, N, 3 + input_channels) tensor
+                Point cloud to run predicts on
+                Each point in the point-cloud MUST
+                be formated as (x, y, z, features...)
+        """
+        _, N, _ = pointcloud.size()
+
+        xyz, features = self._break_up_pc(pointcloud)
+
+        l_xyz, l_features = [xyz], [features]
+        for i in range(len(self.SA_modules)):
+            li_xyz, li_features = self.SA_modules[i](l_xyz[i], l_features[i])
+            l_xyz.append(li_xyz)
+            l_features.append(li_features)
+
+        for i in range(-1, -(len(self.FP_modules) + 1), -1):
+            l_features[i - 1] = self.FP_modules[i](
+                l_xyz[i - 1], l_xyz[i], l_features[i - 1], l_features[i]
+            )
+
+        return l_features[0]
+    
+class SemanticSegmentation(nn.Module):
+    def __init__(self, input_channels=6, num_classes=16):
+        super(SemanticSegmentation, self).__init__()
+
+        # Pointnet2MSG model up to FP modules
+        self.pointnet2msg = Pointnet2MSG(input_channels= 6 )
+
+        # Final classification layer
+        """
+        self.classification_layer = nn.Sequential(
+            nn.Conv1d(128, 256, kernel_size=1),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Conv1d(256, num_classes, kernel_size=1)
+        )
+        """
+        self.classification_layer = nn.Sequential(
+            nn.Conv1d(128, 256, kernel_size=1),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Conv1d(256, 512, kernel_size=1),  # Increased output channels
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(0.5),  # Regularization with dropout
+            nn.Conv1d(512, num_classes, kernel_size=1)
+    )
+
+    def forward(self, pointcloud):
+        # Forward pass through Pointnet2MSG
+        features = self.pointnet2msg(pointcloud)
+
+        # Forward pass through final classification layer
+        per_point_labels = self.classification_layer(features)
+
+        return per_point_labels   

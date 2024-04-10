93c4082360da16f5bbfcde4f1e52dd587de7b6bc
KulunuOS <kulunuds@gmail.com>
2024-04-10 21:23:15 +0300

    .

--- lib/seg_utils.py
+++ lib/seg_utils.py
@@ -4,6 +4,8 @@ import torch
 import torch.nn as nn
 import torch.optim.lr_scheduler as lr_scheduler
 import torch.nn.functional as F
+import numpy as np
+import cv2
 
 def plot_graphs(train_losses, val_losses, train_accuracies, val_accuracies, epoch,fn):
     # Create subplots for loss and accuracy
@@ -113,3 +115,105 @@ class FocalLoss(nn.Module):
 
         return loss
     
+#Function to project depth to pointcloud
+
+def dpt_2_cld( depth_frame, K,segMask = None,cam_scale=1):
+
+    w = depth_frame.shape[1]
+    h = depth_frame.shape[0]
+    
+    xmap = np.array([[j for i in range(w)] for j in range(h)])
+    ymap = np.array([[i for i in range(w)] for j in range(h)])
+    
+    dpt = np.array(depth_frame, dtype=np.float32)
+    dpt = dpt/1000
+    
+    if len(dpt.shape) > 2:
+            dpt = dpt[:, :, 0]
+    msk_dp = dpt > -1
+    choose = msk_dp.flatten().nonzero()[0].astype(np.uint32)
+
+    if len(choose) < 1:
+        return None, None
+        
+    dpt_mskd = dpt.flatten()[choose][:, np.newaxis].astype(np.float32)
+    xmap_mskd = xmap.flatten()[choose][:, np.newaxis].astype(np.float32)
+    ymap_mskd = ymap.flatten()[choose][:, np.newaxis].astype(np.float32)
+    cam_cx, cam_cy = K[0][2], K[1][2]
+    cam_fx, cam_fy = K[0][0], K[1][1]
+    
+    if segMask is not None:
+             
+        focus_points = np.argwhere(segMask != 0)
+        focus = segMask != 0
+        
+        # projecting only the focus
+        pt2 = dpt_mskd[focus.flatten()] / cam_scale
+        pt0b= (ymap_mskd[focus.flatten()] - cam_cx) * pt2 / cam_fx
+        pt1b= (xmap_mskd[focus.flatten()] - cam_cy) * pt2 / cam_fy
+        focus_points = np.concatenate((pt0b, pt1b, pt2),axis=1)
+               
+        return focus_points , choose
+    
+    else :
+
+        # projecting the cloud as a whole    
+        pt2 = dpt_mskd / cam_scale
+        pt0 = (ymap_mskd - cam_cx) * pt2 / cam_fx
+        pt1 = (xmap_mskd - cam_cy) * pt2 / cam_fy
+        cld = np.concatenate((pt0, pt1, pt2),axis=1)
+        
+        return cld , choose
+    
+def project_p3d(p3d, cam_scale, K):
+        
+        if p3d.shape[1]<4:
+            p3d = p3d * cam_scale
+            p2d = np.dot(p3d, K.T)
+            p2d_3 = p2d[:, 2]
+            p2d_3[np.where(p2d_3 < 1e-8)] = 1.0
+            p2d[:, 2] = p2d_3
+            p2d = np.around((p2d[:, :2] / p2d[:, 2:])).astype(np.int32)
+            return p2d
+        else:
+            p3d = p3d * cam_scale
+            #print(p3d.shape)
+            print('xyz_rgb points projected to 2D')
+            p2d = np.dot(p3d[: , 0:3], K.T)
+            p2d_3 = p2d[:, 2]
+            filter = np.where(p2d_3 < 1e-8)
+            if filter[0].shape[0]>0:
+                p2d_rgbs = p3d[filter, 3:6]
+                p2d_3[filter] = 1.0
+            else:
+                p2d_rgbs = p3d[:, 3:6]
+            p2d[:, 2] = p2d_3
+            p2d = np.around((p2d[:, :2] / p2d[:, 2:])).astype(np.int32)
+            
+            #print(p3d.shape)
+            #print(p2d.shape)
+
+            #return np.concatenate((p2d, p2d_rgbs), axis=1).astype(np.int32)
+            return p2d
+
+def draw_p2ds( img, p2ds, color, rad):
+        h, w = img.shape[0], img.shape[1]
+
+        for pt_2d in p2ds:
+            pt_2d[0] = np.clip(pt_2d[0], 0, w)
+            pt_2d[1] = np.clip(pt_2d[1], 0, h)
+
+            if p2ds.shape[1]>2:
+                img = cv2.circle(
+                    cv2.UMat(img), (pt_2d[0], pt_2d[1]), rad, (int(pt_2d[2]), int(pt_2d[3]), int(pt_2d[4])) , -1
+                )
+            else:
+                img = cv2.circle(
+                    cv2.UMat(img), (pt_2d[0], pt_2d[1]), rad, color, -1
+                )
+            '''
+            img = cv2.circle(
+                img, (pt_2d[0], pt_2d[1]), rad, color, -1
+            )'''
+          
+        return img.get()
